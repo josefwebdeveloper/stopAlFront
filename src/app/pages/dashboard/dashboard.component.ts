53f1808375309e0384672f0438b210d0056fcace
Josef Shainskiy <josef2011@gmail.com>
2024-12-22 20:17:03 +0200

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { AddDataPopupComponent } from '../../components/add-data-popup/add-data-popup.component';
+import { AuthService } from '../../services/auth.service';
+import { EntryData } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,18 +16,42 @@ import { AddDataPopupComponent } from '../../components/add-data-popup/add-data-
     MatIconModule
   ]
 })
-export class DashboardComponent {
-  constructor(private dialog: MatDialog) {}
+export class DashboardComponent implements OnInit {
+  dashboardData: EntryData[] = [];
+
+  constructor(
+    private dialog: MatDialog,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.loadDashboardData();
+  }
+
+  private loadDashboardData() {
+    // TODO: Implement getEntries endpoint and service method
+    // this.authService.getEntries().subscribe((entries: EntryData[]) => {
+    //   this.dashboardData = entries;
+    // });
+  }
 
   openAddDataPopup() {
     const dialogRef = this.dialog.open(AddDataPopupComponent, {
       width: '400px'
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result: EntryData | undefined) => {
       if (result) {
-        // Handle the result - maybe refresh dashboard data
-        console.log('Entry added:', result);
+        this.authService.addEntry(result).subscribe({
+          next: (response: EntryData) => {
+            console.log('Entry added successfully:', response);
+            this.loadDashboardData();
+          },
+          error: (error) => {
+            console.error('Error adding entry:', error);
+            // TODO: Add error handling/user notification
+          }
+        });
       }
     });
   }
