feb35f3c0a5792c9f16c4aa5759a6ade1c8cc707
Josef Shainskiy <josef2011@gmail.com>
2024-12-22 23:38:42 +0200

    e

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import {Routes} from '@angular/router';
-import {AuthCallbackComponent} from './auth-callback/auth-callback.component';
+import {AuthCallbackComponent} from './components/auth-callback/auth-callback.component';
 import {AuthGuard} from './guards/auth.guard';
 import {LoginComponent} from './pages/login/login.component';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
