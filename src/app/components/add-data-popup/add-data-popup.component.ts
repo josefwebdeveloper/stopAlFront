843c3df20f238484234e7785e27dbdd0005382be
Josef Shainskiy <josef2011@gmail.com>
2024-12-22 19:45:47 +0200

    upd

--- src/app/components/add-data-popup/add-data-popup.component.ts
+++ src/app/components/add-data-popup/add-data-popup.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {MatDialogRef, MatDialogTitle} from '@angular/material/dialog';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-add-data-popup',
   templateUrl: './add-data-popup.component.html',
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatCheckbox,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatButton,
+    MatLabel
+  ],
   styleUrls: ['./add-data-popup.component.scss']
 })
 export class AddDataPopupComponent {
@@ -33,4 +51,4 @@ export class AddDataPopupComponent {
   onCancel() {
     this.dialogRef.close();
   }
-} 
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,6 +6,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AddDataPopupComponent } from '../add-data-popup/add-data-popup.component';
 
 @Component({
   selector: 'app-header',
@@ -18,15 +21,40 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatButtonModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule
   ]
 })
 export class HeaderComponent {
   @Output() toggleSidenav = new EventEmitter<void>();
   
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private dialog: MatDialog
+  ) {}
 
   onLogout() {
     this.authService.logout();
+  }
+
+  openAddDataPopup() {
+    const dialogRef = this.dialog.open(AddDataPopupComponent, {
+      width: '400px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.authService.addEntry(result).subscribe({
+          next: () => {
+            // Handle success - maybe show a snackbar
+            console.log('Entry added successfully');
+          },
+          error: (error) => {
+            // Handle error - show error message
+            console.error('Error adding entry:', error);
+          }
+        });
+      }
+    });
   }
 } 
